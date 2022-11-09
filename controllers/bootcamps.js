b69ab1c2900049485e589d83591abfeaa01bdd07
Mimo <mimoriam420@gmail.com>
2022-11-09 16:34:32 +0500

    Commit #8 - GET & GET one Bootcamp(s) controller method naive implementation

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,13 +4,33 @@ const Bootcamp = require('../models/Bootcamp');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = async (req, res, next) => {
-    await res.status(200).json({success: true});
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        });
+    } catch (e) {
+        res.status(400).json({success: false});
+    }
 }
 
 // @desc      Get single bootcamp
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({success: false});
+        }
+
+        res.status(200).json({success: true, data: bootcamp});
+    } catch (e) {
+        res.status(400).json({success: false});
+    }
 }
 
 // @desc      Create new bootcamp
