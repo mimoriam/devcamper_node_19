1ede5f8ef0ec6c0c135dc73408d15cb3927293d7
Mimo <mimoriam420@gmail.com>
2022-11-11 23:18:11 +0500

    Commit #17 - Pagination support (?page=2)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -36,11 +36,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt');
     }
 
+    //Pagination:
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25; // This is the amount of results per page shown
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
     const bootcamps = await query;
+
+    const pagination = {};
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit: limit
+        }
+    }
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
+        pagination: pagination,
         data: bootcamps
     });
 })
