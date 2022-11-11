2fa0d7418b89952e0bc4577d7336e101f1e28077
Mimo <mimoriam420@gmail.com>
2022-11-11 22:53:59 +0500

    Commit #15 - Naive filtering using req.query params (?location.city=Kingston&careers[in]=Business)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,16 @@ const geocoder = require('../utils/geocoder');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    console.log(queryStr);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
 
     res.status(200).json({
         success: true,
@@ -109,7 +118,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 // @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
 // @access    Private
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-    const { zipcode, distance } = req.params;
+    const {zipcode, distance} = req.params;
 
     // Get lat/lng from geocoder
     const loc = await geocoder.geocode(zipcode);
@@ -122,7 +131,7 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     const radius = distance / 3963;
 
     const bootcamps = await Bootcamp.find({
-        location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+        location: {$geoWithin: {$centerSphere: [[lng, lat], radius]}}
     });
 
     res.status(200).json({
