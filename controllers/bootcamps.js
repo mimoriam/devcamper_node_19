2e63b62210510de0eea57878b00ec0936036f53e
Mimo <mimoriam420@gmail.com>
2022-11-11 23:08:46 +0500

    Commit #16 - Implement Selection & Sorting (?select=name,description,housing&sort=name)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+// noinspection SillyAssignmentJS
+
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
@@ -9,12 +11,30 @@ const geocoder = require('../utils/geocoder');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
 
-    let queryStr = JSON.stringify(req.query);
+    const reqQuery = {...req.query};
+    const removeFields = ['select'];
+
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    let queryStr = JSON.stringify(reqQuery);
 
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    console.log(queryStr);
 
     query = Bootcamp.find(JSON.parse(queryStr));
+
+    // Selection:
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sorting:
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
 
     const bootcamps = await query;
 
