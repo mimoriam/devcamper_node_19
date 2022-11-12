a3e81852c7539363bbc1994ccdfbf4ca5607dc93
Mimo <mimoriam420@gmail.com>
2022-11-12 23:59:06 +0500

    Commit #23 - Reverse population of Courses in Bootcamp model

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -3,103 +3,107 @@ const slugify = require('slugify');
 const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name can not be more than 50 characters']
-    },
-    slug: String,
-    description: {
-        type: String,
-        required: [true, 'Please add a description'],
-        maxlength: [500, 'Description can not be more than 500 characters']
-    },
-    website: {
-        type: String,
-        match: [
-            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-            'Please use a valid URL with HTTP or HTTPS'
-        ]
-    },
-    phone: {
-        type: String,
-        maxlength: [20, 'Phone number can not be longer than 20 characters']
-    },
-    email: {
-        type: String,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid email'
-        ]
-    },
-    address: {
-        type: String,
-        required: [true, 'Please add an address']
-    },
-    location: {
-        // GeoJSON Point
-        type: {
+        name: {
             type: String,
-            enum: ['Point']
+            required: [true, 'Please add a name'],
+            unique: true,
+            trim: true,
+            maxlength: [50, 'Name can not be more than 50 characters']
         },
-        coordinates: {
-            type: [Number],
-            index: '2dsphere'
+        slug: String,
+        description: {
+            type: String,
+            required: [true, 'Please add a description'],
+            maxlength: [500, 'Description can not be more than 500 characters']
         },
-        formattedAddress: String,
-        street: String,
-        city: String,
-        state: String,
-        zipcode: String,
-        country: String
-    },
-    careers: {
-        // Array of strings
-        type: [String],
-        required: true,
-        enum: [
-            'Web Development',
-            'Mobile Development',
-            'UI/UX',
-            'Data Science',
-            'Business',
-            'Other'
-        ]
-    },
-    // This won't be inserted in the request. It'll be generated using functions
-    averageRating: {
-        type: Number,
-        min: [1, 'Rating must be at least 1'],
-        max: [10, 'Rating must can not be more than 10']
-    },
-    averageCost: Number,
-    photo: {
-        type: String,
-        default: 'no-photo.jpg'
-    },
-    housing: {
-        type: Boolean,
-        default: false
-    },
-    jobAssistance: {
-        type: Boolean,
-        default: false
-    },
-    jobGuarantee: {
-        type: Boolean,
-        default: false
-    },
-    acceptGi: {
-        type: Boolean,
-        default: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-});
+        website: {
+            type: String,
+            match: [
+                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+                'Please use a valid URL with HTTP or HTTPS'
+            ]
+        },
+        phone: {
+            type: String,
+            maxlength: [20, 'Phone number can not be longer than 20 characters']
+        },
+        email: {
+            type: String,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                'Please add a valid email'
+            ]
+        },
+        address: {
+            type: String,
+            required: [true, 'Please add an address']
+        },
+        location: {
+            // GeoJSON Point
+            type: {
+                type: String,
+                enum: ['Point']
+            },
+            coordinates: {
+                type: [Number],
+                index: '2dsphere'
+            },
+            formattedAddress: String,
+            street: String,
+            city: String,
+            state: String,
+            zipcode: String,
+            country: String
+        },
+        careers: {
+            // Array of strings
+            type: [String],
+            required: true,
+            enum: [
+                'Web Development',
+                'Mobile Development',
+                'UI/UX',
+                'Data Science',
+                'Business',
+                'Other'
+            ]
+        },
+        // This won't be inserted in the request. It'll be generated using functions
+        averageRating: {
+            type: Number,
+            min: [1, 'Rating must be at least 1'],
+            max: [10, 'Rating must can not be more than 10']
+        },
+        averageCost: Number,
+        photo: {
+            type: String,
+            default: 'no-photo.jpg'
+        },
+        housing: {
+            type: Boolean,
+            default: false
+        },
+        jobAssistance: {
+            type: Boolean,
+            default: false
+        },
+        jobGuarantee: {
+            type: Boolean,
+            default: false
+        },
+        acceptGi: {
+            type: Boolean,
+            default: false
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+    }, {
+        toJSON: {virtuals: true},
+        toObject: {virtuals: true}
+    }
+);
 
 // Create Slug:
 BootcampSchema.pre('save', function (next) {
@@ -124,6 +128,14 @@ BootcampSchema.pre('save', async function (next) {
     // Do not save address in DB
     this.address = undefined;
     next();
+});
+
+// Reverse populate Bootcamp with Courses JSON:
+BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
