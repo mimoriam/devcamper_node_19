affb98469f963515fea8c61ba4f5698e6afa6515
Mimo <mimoriam420@gmail.com>
2022-11-12 23:09:50 +0500

    Commit #21 - Create a merged route (/api/v1/bootcamps/:bootcampId/courses)

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius} = require("../controllers/bootcamps");
 
+// Include course router for path: /api/v1/bootcamps/:bootcampId/courses
+const courseRouter = require('./courses');
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/').get(getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
