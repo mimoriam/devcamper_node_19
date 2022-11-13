576f2729acb03a27a747116c3546edb0e955b05f
Mimo <mimoriam420@gmail.com>
2022-11-13 17:01:13 +0500

    Commit #31.5 - just some import removal

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,7 +1,6 @@
 const Course = require('../models/Course');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
-const geocoder = require('../utils/geocoder');
 const Bootcamp = require("../models/Bootcamp");
 
 // @desc      Get all courses
