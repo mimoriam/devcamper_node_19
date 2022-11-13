d4e67e694206d196a8226f12cf3d7d2dd91ad673
Mimo <mimoriam420@gmail.com>
2022-11-13 16:55:38 +0500

    Commit #31 - Implemented advanced results for courses route/controller

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 const {getCourses, getCourse, addCourse, updateCourse, deleteCourse} = require("../controllers/courses");
 
-router.route('/').get(getCourses).post(addCourse);
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
+router.route('/').get(advancedResults(Course, {
+    path: 'bootcamp',
+    select: 'name description'
+}), getCourses).post(addCourse);
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
