8040976d63f3d6cf67d4fe2a394d112a3921015f
Mimo <mimoriam420@gmail.com>
2022-11-13 17:46:10 +0500

    Commit #33 - !!!IMPORTANT - Use logger/morgan only if in development

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
