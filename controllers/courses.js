f4725da95191326f710b2e3a3189e8703b567c07
Mimo <mimoriam420@gmail.com>
2022-11-13 14:31:32 +0500

    Commit #25 - Confusing  (Get single course + Add new course methods implemented)

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,6 +2,7 @@ const Course = require('../models/Course');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc      Get all courses
 // @route     GET /api/v1/courses
@@ -26,4 +27,44 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
         count: courses.length,
         data: courses
     })
+});
+
+// @desc      Get single course
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if (!course) {
+        return next(
+            new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+        );
+    }
+
+    res.status(200).json({success: true, data: course});
+});
+
+// @desc      Add new course
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.bootcampId}`, 404)
+        );
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: course
+    });
 });
