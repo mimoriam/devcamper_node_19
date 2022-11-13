ea15b401bf74db03452ff9d55acce4038620896a
Mimo <mimoriam420@gmail.com>
2022-11-13 14:48:41 +0500

    Commit #26 - Update course

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {getCourses, getCourse, addCourse} = require("../controllers/courses");
+const {getCourses, getCourse, addCourse, updateCourse} = require("../controllers/courses");
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse);
 
 module.exports = router;
