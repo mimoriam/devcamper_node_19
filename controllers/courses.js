4f9725d12f3d83bb3a822c5827b4bec27439414b
Mimo <mimoriam420@gmail.com>
2022-11-13 14:54:14 +0500

    Commit #27 - Delete Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -91,3 +91,32 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         data: course
     });
 });
+
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+        );
+    }
+
+    // // Make sure user is bootcamp owner
+    // if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    //     return next(
+    //         new ErrorResponse(
+    //             `User ${req.user.id} is not authorized to delete this bootcamp`,
+    //             401
+    //         )
+    //     );
+    // }
+
+    await course.remove();
+
+    res.status(200).json({success: true, data: {}});
+
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {getCourses, getCourse, addCourse, updateCourse} = require("../controllers/courses");
+const {getCourses, getCourse, addCourse, updateCourse, deleteCourse} = require("../controllers/courses");
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
