a2be15d3c63c95fc743b6acbbc17f49e881effd3
Mimo <mimoriam420@gmail.com>
2022-11-13 18:01:36 +0500

    Commit #35 - Generate a JWToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,12 +6,18 @@ const asyncHandler = require('../middleware/async');
 // @route     GET /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-    const { name, email, password, role } = req.body;
+    const {name, email, password, role} = req.body;
 
     // Create User:
     const user = await User.create({
         name, email, password, role
     });
 
-    res.status(200).json({success: true})
+    // Create token by using function from User Model:
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token: token
+    });
 });
