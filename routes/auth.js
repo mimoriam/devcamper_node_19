2ae32bce9cfeee2bb859df5c7ac134cca8c73681
Mimo <mimoriam420@gmail.com>
2022-11-13 17:54:30 +0500

    Commit #34 - Register User + Hash Password

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const {register} = require("../controllers/auth");
 const router = express.Router();
 
-router.route('/').get(register);
+router.route('/register').post(register);
 
 module.exports = router;
