67398e80953cc2361cb34f964b06ac6965ffe6c5
Mimo <mimoriam420@gmail.com>
2022-11-13 19:45:38 +0500

    Commit #36 - Login Route/Controller implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 // @desc      Register user
-// @route     GET /api/v1/auth/register
+// @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
     const {name, email, password, role} = req.body;
@@ -12,6 +12,40 @@ exports.register = asyncHandler(async (req, res, next) => {
     const user = await User.create({
         name, email, password, role
     });
+
+    // Create token by using function from User Model:
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token: token
+    });
+});
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate emil & password
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
 
     // Create token by using function from User Model:
     const token = user.getSignedJwtToken();
--- models/User.js
+++ models/User.js
@@ -44,9 +44,14 @@ UserSchema.pre('save', async function (next) {
 
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE,
     });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 
