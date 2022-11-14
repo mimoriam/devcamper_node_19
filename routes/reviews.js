b2e1d2992e25713d52cca6fe263bb032060bca0f
Mimo <mimoriam420@gmail.com>
2022-11-14 17:01:46 +0500

    Commit #50 - Get Single Review

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,12 +6,16 @@ const router = express.Router({ mergeParams: true });
 
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
-const { getReviews } = require("../controllers/reviews");
+const { getReviews, getReview } = require("../controllers/reviews");
 
 router.route('/').get(advancedResults(Review, {
         path: 'bootcamp',
         select: 'name description'
     }), getReviews
 );
+
+router
+    .route('/:id')
+    .get(getReview);
 
 module.exports = router;
