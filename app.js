8cd4ae4157891c34c17e80e13077ad35113020f2
Mimo <mimoriam420@gmail.com>
2022-11-14 17:41:14 +0500

    Commit #54 - Installed numerous packages for vulnerability protection

--- app.js
+++ app.js
@@ -7,11 +7,28 @@
 // npm i jsonwebtoken bcryptjs
 // npm i nodemailer
 
+// Vulnerabilities:
+// npm i express-mongo-sanitize
+// npm i helmet
+// npm i xss-clean
+// npm i express-rate-limit
+// npm i hpp
+// npm i cors
+
 const createError = require('http-errors');
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+
+// Vulnerability protection:
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
 const logger = require('morgan');
 const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
@@ -46,6 +63,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express-fileupload:
 app.use(fileupload({}));
+
+// Reduce Fingerprinting:
+app.disable('x-powered-by')
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+// Limit to 100 requests per 10 mins from same IP:
+// Documentation: Limit certain routes possible too:
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Mount Routers:
 app.use('/api/v1/bootcamps', bootcampRouter);
